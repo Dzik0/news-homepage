503655327ede361c3e000718de794aa24f7096bd
Piotr <hunter976@gmail.com>
2025-09-10 16:03:12 +0200

    changed paths to images

--- src/componenents/Ranking.jsx
+++ src/componenents/Ranking.jsx
@@ -3,19 +3,19 @@ import SingleRankingItem from "./SingleRankingItem";
 export default function Ranking() {
   const data = [
     {
-      image: "./public/image-retro-pcs.jpg",
+      image: "/image-retro-pcs.jpg",
       number: "01",
       title: "Reviving Retro PCs",
       para: "What happens when old PCs are given modern upgrades?",
     },
     {
-      image: "./public/image-top-laptops.jpg",
+      image: "/image-top-laptops.jpg",
       number: "02",
       title: "Top 10 Laptops of 2022",
       para: "Our best picks for various needs and budgets",
     },
     {
-      image: "./public/image-gaming-growth.jpg",
+      image: "/image-gaming-growth.jpg",
       number: "03",
       title: "THe Growth of Gaming",
       para: "How the pandemic as sparked fresh opportunites",
